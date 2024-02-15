616a4b4a5382fd0f1d0ac2ed9468217d997fe5c9
Kurtis Rios <kurtis@altruist.com>
2024-02-14 22:28:29 -0800

    Fixed naming convention of App styles

---
+++ src/Poker.tsx
@@ -0,0 +1,14 @@
+import './Poker.css';
+import Table from './Assets/Table';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='table'>
+        <Table />
+      </div>
+    </div>
+  );
+}
+
+export default App;
